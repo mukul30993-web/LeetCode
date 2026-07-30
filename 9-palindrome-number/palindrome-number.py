2969706a6f58026f740459b107ea5dbb94d448ee
MUKUL <mukul30993@gmail.com>
2026-07-30 23:56:51 +0530

    Time: 4 ms (90.49%) | Memory: 12.3 MB (55.00%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+
+        original = x
+        reverse = 0
+
+        while x > 0:
+
+            digit = x % 10
+
+            reverse = reverse * 10 + digit
+
+            x = x // 10
+
+        if original == reverse:
+            return True
+
+        return False
