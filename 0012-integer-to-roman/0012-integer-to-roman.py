3292e5473f72f498dc70562b4ec49234872fa95b
MUKUL <mukul30993@gmail.com>
2026-08-02 23:42:41 +0530

    Time: 4 ms (77.9%), Space: 12.5 MB (20.68%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def intToRoman(self, num):
+        
+        values = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+        symbols = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+
+        answer = ""
+
+        i = 0
+
+        while num > 0:
+
+            while num >= values [i]:
+                answer += symbols[i]
+                num -= values[i]
+
+            i += 1
+
+        return answer
+        
