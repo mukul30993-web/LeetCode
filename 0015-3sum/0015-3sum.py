5ac2eda5f3263ff551535fc096d556aff01bd287
MUKUL <mukul30993@gmail.com>
2026-08-06 00:04:50 +0530

    Time: 773 ms (36.71%), Space: 18.3 MB (58.26%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def threeSum(self, nums):
+
+        nums.sort()
+        answer = []
+
+        for i in range(len(nums)):
+
+            # Skip duplicate first numbers
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+
+                total = nums[i] + nums[left] + nums[right]
+
+                if total == 0:
+
+                    answer.append([nums[i], nums[left], nums[right]])
+
+                    left += 1
+                    right -= 1
+
+                    # Skip duplicate left values
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+
+                    # Skip duplicate right values
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+                elif total < 0:
+                    left += 1
+
+                else:
+                    right -= 1
+
+        return answer
